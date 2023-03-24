84ff4af4246e5794685816c2143b80f63c1bedc4
syedzubyl <syedzubyl96@gmail.com>
2023-03-24 22:14:03 +0530

    Commit

--- app/src/main/java/abi/parveen/donate/HomeActivity.java
+++ app/src/main/java/abi/parveen/donate/HomeActivity.java
@@ -1,36 +1,81 @@
 package abi.parveen.donate;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class HomeActivity extends AppCompatActivity {
 Button blooddonate ;
 Button fooddonate;
+
+Button outbtn;
+    GoogleSignInOptions googleSignInOptions;
+    GoogleSignInClient googleSignInClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        blooddonate =findViewById(R.id.requestblooddonate);
-        fooddonate =findViewById(R.id.readytoblooddonate);
+        blooddonate = findViewById(R.id.requestblooddonate);
+        fooddonate = findViewById(R.id.readytoblooddonate);
+        outbtn =findViewById(R.id.outbtn);
 
         blooddonate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(HomeActivity.this, BloodDonateActivity.class);
+                Intent intent = new Intent(HomeActivity.this, BloodDonateActivity.class);
                 startActivity(intent);
             }
         });
         fooddonate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent =new Intent(HomeActivity.this,FoodDonateActivity.class);
+                Intent intent = new Intent(HomeActivity.this, FoodDonateActivity.class);
                 startActivity(intent);
             }
         });
+        googleSignInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+        googleSignInClient = GoogleSignIn.getClient(this, googleSignInOptions);
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(this);
+        final SharedPreferences share = getSharedPreferences("Data", Context.MODE_PRIVATE);
+        outbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                SharedPreferences.Editor editt = share.edit();
+                editt.clear();
+                editt.commit();
+
+                signout();
+
+
+            }
+        });
     }
-}
+        private void signout() {
+            googleSignInClient.signOut().addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    finish();
+
+                    startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                    Toast.makeText(HomeActivity.this,"signout successful",Toast.LENGTH_SHORT).show();
+                }
+            });
+    }}
--- app/src/main/java/abi/parveen/donate/newreadytodonateActivity.java
+++ app/src/main/java/abi/parveen/donate/newreadytodonateActivity.java
@@ -3,12 +3,35 @@ package abi.parveen.donate;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class newreadytodonateActivity extends AppCompatActivity {
+public class newreadytodonateActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_newreadytodonate);
+
+        Spinner spinner =findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
+       adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+       spinner.setAdapter(adapter);
+       spinner.setOnItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text =parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(),text,Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
