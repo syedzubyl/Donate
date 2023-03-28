db1868ef4bca9105ae6441cef76fc90973f6ddc5
syedzubyl <syedzubyl96@gmail.com>
2023-03-28 14:21:20 +0530

    Commit

--- app/src/main/java/abi/parveen/donate/newreadytodonateActivity.java
+++ app/src/main/java/abi/parveen/donate/newreadytodonateActivity.java
@@ -3,8 +3,11 @@ package abi.parveen.donate;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.AdapterView;
@@ -12,10 +15,12 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -33,6 +38,8 @@ public class newreadytodonateActivity extends AppCompatActivity implements Adapt
     private EditText location;
     private Button register;
 
+    ProgressDialog progressDialog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +51,31 @@ public class newreadytodonateActivity extends AppCompatActivity implements Adapt
         location =findViewById(R.id.reaadylocation);
         spinner =findViewById(R.id.spinner);
         register =findViewById(R.id.readyregister);
+
+        progressDialog=new ProgressDialog(this);
+
+        BottomNavigationView newbottmNavigationView =findViewById(R.id.newNavigationView);
+
+
+        newbottmNavigationView.setSelectedItemId(R.id.newaccount);
+
+        newbottmNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()){
+
+                    case R.id.newaccount:
+                        return true;
+                    case R.id.home:
+                        startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+
+
+                }
+                return false;
+            }
+        });
 
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -63,12 +95,27 @@ public class newreadytodonateActivity extends AppCompatActivity implements Adapt
                 hashMap.put("email",getbloodgroup);
                 hashMap.put("location",getlocation);
 
+                if(getname.isEmpty()){
+                    readyname.setError("Please Enter the user name");
+                }
+                else if(getlocation.isEmpty()){
+                    location.setError("Please Enter the location");
+                }
+                else if(getbloodgroup.isEmpty()){
+                    email.setError("Please Enter the Email");
+                }
+                else{
+                    progressDialog.setMessage("Please wait while Register");
+                    progressDialog.setTitle("Register");
+                    progressDialog.setCanceledOnTouchOutside(false);
+                    progressDialog.show();
                 // Check if the user already exists in the database
                 myRef.child("user").orderByChild("name").equalTo(getname).addListenerForSingleValueEvent(new ValueEventListener() {
                     @Override
                     public void onDataChange(DataSnapshot dataSnapshot) {
                         if (dataSnapshot.exists()) {
                             // User with the same name already exists
+                            progressDialog.dismiss();
                             Toast.makeText(newreadytodonateActivity.this, "User with this name already exists", Toast.LENGTH_SHORT).show();
                         } else {
                             // Check if the user already exists based on email, blood group, and location
@@ -84,12 +131,14 @@ public class newreadytodonateActivity extends AppCompatActivity implements Adapt
                                                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                     @Override
                                                     public void onSuccess(Void unused) {
+                                                        progressDialog.dismiss();
                                                         Toast.makeText(newreadytodonateActivity.this,"Congratulations, successfully registered",Toast.LENGTH_SHORT).show();
                                                     }
                                                 })
                                                 .addOnFailureListener(new OnFailureListener() {
                                                     @Override
                                                     public void onFailure(@NonNull Exception e) {
+                                                        progressDialog.dismiss();
                                                         Toast.makeText(newreadytodonateActivity.this,"Something went wrong: " + e.getMessage(),Toast.LENGTH_SHORT).show();
                                                     }
                                                 });
@@ -109,7 +158,7 @@ public class newreadytodonateActivity extends AppCompatActivity implements Adapt
                         // Handle databaseError
                     }
                 });
-            }
+            }}
         });
         ArrayAdapter<CharSequence> adapter =ArrayAdapter.createFromResource(this, R.array.numbers, android.R.layout.simple_spinner_item);
        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
